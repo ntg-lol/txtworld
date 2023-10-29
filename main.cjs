51ab497c03409e44ffd75aadb1553b271fcf1c72
ntgowot <jb3499711@gmail.com>
2023-10-29 18:28:12 +0000

    improve logs, tidy up

--- main.cjs
+++ main.cjs
@@ -1,21 +1,29 @@
+console.log("[start] starting up...")
+
+console.log("[start] importing modules...")
 const express          = require("express");
 const { createServer } = require("http");
 const { Server }       = require("socket.io");
 
+console.log("[start] creating server...")
 const app     = express();
 const server  = createServer(app);
 const io      = new Server(server);
 
+console.log("[start] creating middleware...")
 app.use("/static", express.static(__dirname + "/client/"));
 
-app.get("/", (req, res)=>{
+console.log("[start] sending index.html file...")
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/client/index.html");
 });
 
+console.log("[start] creating socket.io listener...")
 io.on("connection", (socket) => {
-    console.log("connection");
+    console.log("[socket] connection");
 });
 
-app.listen(3000, ()=>{
-    console.log("started on 3000");
+console.log("[start] listening...")
+app.listen(3000, () => {
+    console.log("[start] started on 3000");
 });
