bf58a60e584b135a047353192e38253d3e6f55c2
ntgowot <jb3499711@gmail.com>
2023-11-08 17:15:59 +0000

    add cursor rectangle, modularise

--- client/canvas.cjs
+++ client/canvas.cjs
@@ -1,5 +1,12 @@
 const c   = document.getElementById("canvas");
 const ctx = c.getContext("2d");
 
-ctx.font = "18px Consolas";
-ctx.fillText("it works", 10, 20);
+function render() {
+    ctx.fillStyle = "#ffff00";
+    ctx.fillRect(10, 6, 8, 18);
+    ctx.fillStyle = "#000000";
+    ctx.font = "18px Consolas";
+    ctx.fillText("hello", 10, 20);
+}
+
+render();
