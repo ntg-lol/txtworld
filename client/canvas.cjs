82b2c879bca8fd0e0e130bf4dbc48d5778b24eb0
ntgowot <jb3499711@gmail.com>
2023-11-08 17:36:47 +0000

    improve design

--- client/canvas.cjs
+++ client/canvas.cjs
@@ -2,9 +2,9 @@ const c   = document.getElementById("canvas");
 const ctx = c.getContext("2d");
 
 function render() {
-    ctx.fillStyle = "#ffff00";
+    ctx.fillStyle = "#fabd2f";
     ctx.fillRect(10, 6, 8, 18);
-    ctx.fillStyle = "#000000";
+    ctx.fillStyle = "#ebdbb2";
     ctx.font = "18px Consolas";
     ctx.fillText("hello", 10, 20);
 }
