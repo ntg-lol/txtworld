fe36e989821264877ca15dbe62ecb29c8fc65474
ntgowot <jb3499711@gmail.com>
2023-12-17 16:14:48 +0000

    create world.write() function

--- client/canvas.cjs
+++ client/canvas.cjs
@@ -1,12 +1,26 @@
 const c   = document.getElementById("canvas");
 const ctx = c.getContext("2d");
 
-function render() {
-    ctx.fillStyle = "#83a598";
-    ctx.fillRect(10, 6, 10, 18);
-    ctx.fillStyle = "#ebdbb2";
-    ctx.font = "18px Consolas";
-    ctx.fillText("hello", 10, 20);
+let xInPixels;
+let yInPixels;
+
+const world = {
+    render: function() {
+        ctx.clearRect(0, 0, 10000, 10000);
+        xInPixels = 10;
+        yInPixels = 6;
+        ctx.fillStyle = "#83a598";
+        ctx.fillRect(xInPixels, yInPixels, xInPixels, yInPixels + 12);
+        ctx.fillStyle = "#ebdbb2";
+        ctx.font = "18px Consolas";
+        // world.typeString("hello", xInPixels, yInPixels + 14);
+        world.write("hello", xInPixels, yInPixels + 14);
+        world.write("!", xInPixels, yInPixels + 14);
+    },
+    write: function(string, x, y) {
+        ctx.fillText(string, x, y);
+        xInPixels += 10 * string.length;
+    },
 }
 
-render();
+world.render();
