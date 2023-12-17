460d19065d2f358e30c4b787239052596d372af0
ntgowot <jb3499711@gmail.com>
2023-12-17 15:51:06 +0000

    change colours, change cursor size

--- client/canvas.cjs
+++ client/canvas.cjs
@@ -2,8 +2,8 @@ const c   = document.getElementById("canvas");
 const ctx = c.getContext("2d");
 
 function render() {
-    ctx.fillStyle = "#fabd2f";
-    ctx.fillRect(10, 6, 8, 18);
+    ctx.fillStyle = "#83a598";
+    ctx.fillRect(10, 6, 10, 18);
     ctx.fillStyle = "#ebdbb2";
     ctx.font = "18px Consolas";
     ctx.fillText("hello", 10, 20);
